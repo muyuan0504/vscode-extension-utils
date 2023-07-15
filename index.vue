128f98848aa344f760ea4b0e66f95db21445cfcf
AidenPeng <972523881@qq.com>
2023-07-15 15:12:38 +0800

    fix: 代码片段

---
+++ index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    props: [],
+    components: {},
+    watch: {},
+    data() {
+        return {}
+    },
+    computed() {},
+    created() {},
+    methods: {},
+}
+</script>
+<style lang="scss" scoped>
+.page {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    word-break: break-all;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2; /* 这里是超出几行省略 */
+}
+</style>
